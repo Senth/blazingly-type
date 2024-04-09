484020f3ee479849a69d95b7b4223f7811bb1740
senth <senth.wallace@gmail.com>
2024-04-09 20:30:56 +0200

    Use hook instead of signin and save the user

--- src/auth/authServices.ts
+++ src/auth/authServices.ts
@@ -3,23 +3,39 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signInWithEmailAndPassword,
+  User,
 } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const provider = new GoogleAuthProvider();
 
-export function signInWithGoogle() {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // Google sign-in was successful.
-      console.log(result.user);
-    })
-    .catch((error) => {
-      // Handle errors here.
-      console.error(error);
-    });
+export interface AuthSignIn {
+  signIn: () => boolean;
 }
 
-export function signInWithEmail(email: string, password: string) {
+export function useSignInWithGoogle(): AuthSignIn {
+  const navigate = useNavigate();
+
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        navigate("/");
+        return true;
+      })
+      .catch((error) => {
+        // Handle errors here.
+        console.error(error);
+        return false;
+      });
+
+    return true;
+  };
+  return { signIn };
+}
+
+export async function signInWithEmail(email: string, password: string) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
@@ -29,4 +45,27 @@ export function signInWithEmail(email: string, password: string) {
       // Handle Errors here.
       console.error(error);
     });
+}
+
+export function useAuthState() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+
+      // Save or clear the user data in the local storage
+      if (user) {
+        localStorage.setItem("user", JSON.stringify(user));
+      } else {
+        localStorage.removeItem("user");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return { user, loading };
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,11 @@
-import { signInWithEmail, signInWithGoogle } from "@auth";
-import React, { useState } from "react";
+import { useSignInWithGoogle } from "@auth";
 
 export default function LoginPage(): JSX.Element {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const { signIn } = useSignInWithGoogle();
   return (
     <div>
       <h1 className="text-white">Login</h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button onClick={() => signInWithEmail(email, password)}>Sign in</button>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <button onClick={signIn}>Sign in with Google</button>
     </div>
   );
 }
