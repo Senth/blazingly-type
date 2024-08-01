8b0ec73b0f1b4132b81bbb2cdbedffe8c045132b
senth <senth.wallace@gmail.com>
2024-08-01 21:36:45 +0200

    Add settings and link menu link

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TypingPracticePage from "@pages/TypingPractice";
 import Modals from "@components/Modals";
+import Settings from "@pages/Settings";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/settings" element={<Settings />} />
           <Route path="/" element={<TypingPracticePage />} />
         </Routes>
       </Router>
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,9 @@
+import TopBar from "@components/TopBar";
+
+export default function Settings(): JSX.Element {
+  return (
+    <div className="h-full w-full flex flex-col">
+      <TopBar />
+    </div>
+  );
+}
