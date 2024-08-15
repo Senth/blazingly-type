f7a686e051fef9fddc261a491ac058b0be1c597a
senth <senth.wallace@gmail.com>
2024-08-15 11:43:48 +0200

    Add licence information to dbCacheMiddleware

--- src/stores/dbCacheMiddleware.ts
+++ src/stores/dbCacheMiddleware.ts
@@ -1,3 +1,33 @@
+// Purpose: Middleware to persist the state to both the Firestore database and the local storage.
+// The local storage is used to cache the state and the Firestore database is used to let the
+// user access the state from different devices.
+//
+// This code is a modified version of the original code from zustand persist middleware.
+// The original code can be found at: https://github.com/pmndrs/zustand/blob/main/src/middleware/persist.ts
+//
+// MIT License
+//
+// Copyright (c) 2019 Paul Henschel, original zustand persist middleware
+// Copyright (c) 2024 Matteus Magnusson, Firestore and cache middleware
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import { StateCreator, StoreApi, StoreMutatorIdentifier } from "zustand";
 
 export interface StateStorage {
